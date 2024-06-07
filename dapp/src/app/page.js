641f7811acaa169073d395d694551d1b5204ef64
LuizTools <luizfduartejr@gmail.com>
2024-06-06 23:41:53 -0300

    class 3

--- dapp/src/app/page.js
+++ dapp/src/app/page.js
@@ -1,7 +1,36 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { getOpenRequests } from "@/services/Web3Service";
+import Request from "@/components/Request";
 
 export default function Home() {
+
+  const [requests, setRequests] = useState([]);
+
+  useEffect(() => {
+    loadRequests(lastId);
+  }, [])
+
+  async function loadRequests(lastId) {
+    try {
+      const result = await getOpenRequests(lastId);
+      if (lastId === 0)
+        setRequests(result);
+      else {
+        requests.push(...result);
+        setRequests(requests);
+      }
+    }
+    catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  }
+
   return (
     <>
       <Header />
@@ -10,7 +39,13 @@ export default function Home() {
           <p className="lead m-4">Ajude as vítimas de enchentes e demais desastres naturais do Brasil.</p>
         </div>
         <div className="p-4 mx-5">
-
+          <div className="list-group">
+            {
+              requests && requests.length
+                ? requests.map(rq => <Request key={rq.id} data={rq} />)
+                : <>Conect sua carteira MetaMask no botão "Entrar" para ajudar ou pedir ajuda.</>
+            }
+          </div>
         </div>
         <Footer />
       </div>
